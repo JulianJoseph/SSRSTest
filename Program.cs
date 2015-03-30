ee5305bb381c1f379035cff7b65ba1ea79806761
Jules Joseph <info@josephtechnology.net>
2015-03-30 21:58:51 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Microsoft.Data.Schema.ScriptDom;
+using Microsoft.Data.Schema.ScriptDom.Sql;
+using NUnit.Framework;
+
+
+
+namespace SSRSTest
+{
+    public class ReportInfo
+    {
+        public string reportInfoPath;
+        public string ssrsUrl;
+
+        public ReportInfo(string r, string ssrs)//constructor accepting path to report
+        {
+            this.reportInfoPath = r;
+            this.ssrsUrl = ssrs;
+        }
+
+        public ReportInfo()
+        {
+            this.reportInfoPath = ConfigurationManager.AppSettings["ReportInfoPath"];
+            #if(DEV)
+                                this.ssrsUrl = ConfigurationManager.AppSettings["DEV_SSRS_WebService"];
+            #elif(PRESIT)
+                                this.ssrsUrl = ConfigurationManager.AppSettings["PRESIT_SSRS_WebService"];
+            #elif(SIT)
+                                  this.ssrsUrl = ConfigurationManager.AppSettings["SIT_SSRS_WebService"];
+            #elif(PROD)
+                                this.ssrsUrl = ConfigurationManager.AppSettings["PROD_SSRS_WebService"];
+            #endif
+        }
+
+        /// <summary>
+        /// See http://www.nunit.org/index.php?p=testCaseSource&r=2.5.9f
+        /// For reference
+        /// Also https://msdn.microsoft.com/en-us/library/9k7k7cf0.aspx
+        /// for info re use of Yield keyword, in summary (from MSDN)
+        /// When you use the yield keyword in a statement, you indicate that the method, operator, 
+        /// or get accessor in which it appears is an iterator. Using yield to define an iterator removes the need 
+        /// for an explicit extra class (the class that holds the state for an enumeration, see IEnumerator<T> for an example) 
+        /// when you implement the IEnumerable and IEnumerator pattern for a custom collection type.
+        /// </summary>
+
+        public IEnumerable GetReportTestCases
+        {
+            get
+            {
+                using (var reports = XmlReader.Create(this.reportInfoPath))
+                {
+                    while (reports.Read())
+                    {
+                        if (reports.Name == "report")
+                        {
+                            int reportNumber;
+                            if (!int.TryParse(reports["filename"].Substring(0, 3), out reportNumber))
+                            {
+                                reportNumber = 0;
+                            }
+
+                            string reportPath = string.Concat(reports["subfolder"].ToString().Length > 0 ? "/" : "", reports["subfolder"], "/", reports.ReadElementContentAsString());
+                            yield return new TestCaseData(reportNumber, reportPath, this.ssrsUrl);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+namespace SSRSTest
+{
+
+
+    [TestFixture]
+    public class ReportRenderTest
+    {
+        public string ROOT_PATH = ConfigurationManager.AppSettings["RootPath"];
+
+        [Test]
+        [TestCaseSource(typeof(ReportInfo), "GetReportTestCases")]
+        [Category("SSRSTest")]
+        public void DoesReportRender(int reportNumber, string reportPath, string ssrsUrl)
+        {
+            Console.WriteLine("Testing report: {0}: {1} @ {2}", reportNumber.ToString("000"), reportPath, DateTime.Now.ToString("dd-MMM-yyyy HH:mm:ss"));
+            bool result = ReportTester.RenderReport(ROOT_PATH + reportPath, ssrsUrl);
+            Assert.AreEqual(true, result);
+        }
+    }
+
+    [TestFixture]
+    public class ReportDeploymentTest
+    {
+        public string ROOT_PATH = ConfigurationManager.AppSettings["RootPath"];
+        [Test]
+        [TestCaseSource(typeof(ReportInfo), "GetReportTestCases")]
+        [Category("SSRSTest")]
+        public void DoesReportExist(int reportNumber, string reportPath, string ssrsUrl)
+        {
+            Console.WriteLine("Testing report: {0}: {1} @ {2}", reportNumber.ToString("000"), reportPath, DateTime.Now.ToString("dd-MMM-yyyy HH:mm:ss"));
+            bool result = ReportTester.ReportExists(ROOT_PATH + reportPath, ssrsUrl);
+            Assert.AreEqual(true, result);
+        }
+
+    }
+
+}
+
+
+
---
+++ ReportTester.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Web.Services.Protocols;
+using SSRSTest.RSWebReference;
+
+
+namespace SSRSTest
+{
+    public static class ReportTester
+    {
+
+        public static bool ReportExists(string reportPath, string ssrsUrl)
+        {
+            var rs = new ReportExecutionService();
+            rs.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            rs.Url = ssrsUrl;
+
+            byte[] result = null;
+            string historyID = null;
+            string devInfo = @"<DeviceInfo><Toolbar>False</Toolbar></DeviceInfo>";
+            string encoding;
+            string mimeType;
+            string extension;
+            Warning[] warnings = null;
+            string[] streamIDs = null;
+            string format = "XML";
+            ParameterValue[] parameters = null;
+
+            ExecutionInfo execInfo = new ExecutionInfo();
+            ExecutionHeader execHeader = new ExecutionHeader();
+
+            rs.ExecutionHeaderValue = execHeader;
+
+            try
+            {
+                execInfo = rs.LoadReport(reportPath, historyID);
+            }
+            catch (SoapException e)
+            {
+                if (e.Detail.OuterXml.Contains("rsItemNotFound"))
+                {
+                    Console.WriteLine("Report: {0} not found", reportPath);
+                }
+                else
+                {
+                    Console.WriteLine(e.Detail.OuterXml);
+                }
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public static bool RenderReport(string reportPath, string ssrsUrl)
+        {
+            var rs = new ReportExecutionService();
+            rs.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            rs.Url = ssrsUrl;
+
+            byte[] result = null;
+            string historyID = null;
+            string devInfo = @"<DeviceInfo><Toolbar>False</Toolbar></DeviceInfo>";
+            string encoding;
+            string mimeType;
+            string extension;
+            Warning[] warnings = null;
+            string[] streamIDs = null;
+            string format = "XML";
+            ParameterValue[] parameters = null;
+
+            ExecutionInfo execInfo = new ExecutionInfo();
+            ExecutionHeader execHeader = new ExecutionHeader();
+
+            rs.ExecutionHeaderValue = execHeader;
+
+            try
+            {
+                execInfo = rs.LoadReport(reportPath, historyID);
+            }
+            catch (SoapException e)
+            {
+                if (e.Detail.OuterXml.Contains("rsItemNotFound"))
+                {
+                    Console.WriteLine("Report: {0} not found", reportPath);
+                }
+                else
+                {
+                    Console.WriteLine(e.Detail.OuterXml);
+                }
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+            //check if this report requires parms
+            if (execInfo.ParametersRequired)
+            {
+                //if it does, then iterate parms and create set of input parms with appropriate values
+                parameters = new ParameterValue[execInfo.Parameters.Length];
+                for (int i = 0; i < execInfo.Parameters.Length; i++)
+                {
+                    if (execInfo.Parameters[i].PromptUser)
+                    {
+                        parameters[i] = new ParameterValue();
+                        parameters[i].Name = execInfo.Parameters[i].Name;
+
+                        if (execInfo.Parameters[i].ValidValues != null && execInfo.Parameters[i].ValidValues.Length > 0)
+                        {
+                            for (int x = 0; x < execInfo.Parameters[i].ValidValues.Length; x++)
+                            {
+                                //problem setting parm value to null from valid values
+                                //iterate till we get a non-null value
+                                if (!string.IsNullOrEmpty(execInfo.Parameters[i].ValidValues[x].Value))
+                                {
+                                    parameters[i].Value = execInfo.Parameters[i].ValidValues[x].Value;
+                                }
+                            }
+                            Console.WriteLine("Setting {0} to {1}", parameters[i].Name, parameters[i].Value);
+                        }
+                        else
+                        {
+                            switch (execInfo.Parameters[i].Type)
+                            {
+                                case ParameterTypeEnum.DateTime:
+                                    parameters[i].Value = DateTime.Now.ToLongDateString();
+                                    break;
+                                case ParameterTypeEnum.Boolean:
+                                    parameters[i].Value = "true";
+                                    break;
+                                case ParameterTypeEnum.Float:
+                                    parameters[i].Value = "1000.00";
+                                    break;
+                                case ParameterTypeEnum.Integer:
+                                    parameters[i].Value = "10";
+                                    break;
+                                case ParameterTypeEnum.String:
+                                    parameters[i].Value = "AAA";
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        Console.WriteLine("---- Parameter: {0}, Value: {1}", parameters[i].Name, parameters[i].Value.ToString());
+                    }
+                }
+            }
+            else
+            {
+                parameters = new ParameterValue[0];
+            }
+
+            rs.SetExecutionParameters(parameters, "en-us");
+            String SessionId = rs.ExecutionHeaderValue.ExecutionID;
+
+            try
+            {
+                rs.Timeout = 300000;
+                result = rs.Render(format, devInfo, out extension, out encoding, out mimeType, out warnings, out streamIDs);
+                execInfo = rs.GetExecutionInfo();
+                Console.WriteLine("Execution date and time: {0}", execInfo.ExecutionDateTime);
+            }
+            catch (SoapException e)
+            {
+                Console.WriteLine(e.Detail.OuterXml);
+                return false;
+            }
+
+
+            return true;
+        }
+    }
+}
